352337710e99155287c9a0d4803095c3e7d5b05e
Axel Bellec <axel.bellec@outlook.fr>
2017-03-26 19:30:28 +0200

    add get_lines classmethod

--- botigo/lacub.py
+++ botigo/lacub.py
@@ -27,16 +27,35 @@ class LacubAPI(object):
 
         response = requests.get('{}/wfs'.format(cls.BASE_URL), params=payload)
         stops = util.load_xml(response.content)
-        return cls.normalize(stops)
+        return cls.normalize_stops(stops)
+
+    @classmethod
+    def get_lines(cls):
+        # http://data.bordeaux-metropole.fr/wfs?key=[VOTRECLE]&REQUEST=GetFeature&SERVICE=WFS&VERSION=1.1.0&TYPENAME=bm:TB_CHEM_L&SRSNAME=EPSG:3945&PROPERTYNAME=NUMEXPLO,LONGCHEM,COULCHEM,NBPARJOH,NBPARJOE,RH_TB_LIGNE,CDATE,MDATE
+
+        payload = {
+            'KEY': cls.API_KEY,
+            'SERVICE': 'WFS',
+            'VERSION': '1.1.0',
+            'REQUEST': 'GetFeature',
+            'TYPENAME': 'bm:TB_CHEM_L',
+            'SRSNAME': 'EPSG:4326'
+        }
+
+        response = requests.get('{}/wfs'.format(cls.BASE_URL), params=payload)
+        lines = util.load_xml(response.content)
+        return cls.normalize_lines(lines)
 
     @staticmethod
-    def normalize(stops):
+    def normalize_stops(stops):
         """ Normalize each feature parsed by BeautifulSoup. """
 
         extract = util.extract_element
         normalized = lambda stop: {
             'libelle': extract(stop, 'bm:libelle'),
             'gid': extract(stop, 'bm:gid'),
+            'ident': extract(stop, 'bm:ident'),
+            'groupe': extract(stop, 'bm:groupe'),
             'latitude': float(extract(stop, 'gml:pos').split(' ')[0]),
             'longitude': float(extract(stop, 'gml:pos').split(' ')[1]),
             'type': extract(stop, 'bm:type'),
@@ -47,6 +66,31 @@ class LacubAPI(object):
         return [
             normalized(stop)
             for stop in stops.find_all('gml:featuremember')
+        ]
+
+    @staticmethod
+    def normalize_lines(lines):
+        """ Normalize each feature parsed by BeautifulSoup. """
+
+        extract = util.extract_element
+        normalized = lambda line: {
+            'nomcomch': extract(line, 'bm:nomcomch'),
+            'nomcomli': extract(line, 'bm:nomcomli'),
+            'gid': extract(line, 'bm:gid'),
+            'ident': extract(line, 'bm:ident'),
+            'idardeb': extract(line, 'bm:idardeb'),
+            'idarfin': extract(line, 'bm:idarfin'),
+            'sens': extract(line, 'bm:sens'),
+            'numexplo': extract(line, 'bm:numexplo'),
+            'rh_tb_ligne': extract(line, 'bm:rh_tb_ligne'),
+            'type': extract(line, 'bm:type'),
+            'cdate': dt.datetime.strptime(extract(line, 'bm:cdate'), DATE_FORMAT).isoformat(),
+            'mdate': dt.datetime.strptime(extract(line, 'bm:mdate'), DATE_FORMAT).isoformat()
+        }
+
+        return [
+            normalized(line)
+            for line in lines.find_all('gml:featuremember')
         ]
 
 
